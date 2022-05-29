e02b30e84c326c4170be9cf6ea0bab5009ed3da1
Emmanuel <oluwatopealofe@gmail.com>
2022-05-29 10:59:50 -0400

    cleaned documentation a little

--- face_block.py
+++ face_block.py
@@ -1,17 +1,19 @@
-# Main face_block file
+'''
+face_block.py contains core function to run FaceBlock
+'''
 
-from ast import Name
+# Package imports: cv2, face_recognition, numpy
 import cv2 as cv
 import face_recognition
 import numpy as np
 
-import read_files # Use the directory names
+# Module imports: read_files - processes the names of image files for ease of access
+import read_files
 
-def face_block():
-    # Iterate thru each image in images/people and store an array known_face_encodings
-    # with all image encodings present of people who don't want their faces in pics
-    known_face_encodings = []
-    known_faces_to_blur = []
+def face_block() -> None:
+    # 1. identify people that want their face blurred from the images/people folder
+    known_face_encodings = [] # saves face encodings of people who want face blurred
+    known_faces_to_blur = [] # will contain true for all faces to blur, will be used as zip pair
     people = read_files.people()
     for person_img in people:
         image = cv.imread(person_img)
@@ -20,8 +22,7 @@ def face_block():
         for encoding in image_encoding:
             known_face_encodings.append(encoding)
             known_faces_to_blur.append(True)
-    
-    # Iterate thru ...
+
     faces_to_blur = []
     pics = read_files.pics()
     for pic_img in pics:
@@ -45,7 +46,7 @@ def face_block():
             if to_blur:
                 # pixelated blur
                 # divide the input image into NxN blocks
-                blocks = 8
+                blocks = 7
                 xSteps = np.linspace(left, right, blocks + 1, dtype="int")
                 ySteps = np.linspace(top, bottom, blocks + 1, dtype="int")
                 # loop over the blocks in both the x and y direction
@@ -64,7 +65,7 @@ def face_block():
                         (B, G, R) = [int(x) for x in cv.mean(roi)[:3]]
                         cv.rectangle(image, (startX, startY), (endX, endY),
                             (B, G, R), -1)
-
+        
         # Display the resulting image
         cv.imshow('Image', image)
         cv.waitKey(0)
--- read_files.py
+++ read_files.py
@@ -1,17 +1,26 @@
-# Helper module that assists with reading the names of each file in the people
-# and pics folders so that navigating and interacting with images within will be
-# easier in face_block.py
+'''
+read_files.py module functions assists with reading the names of each file in the people
+and pics folders so that navigating and interacting with images within will be
+easier in face_block.py
+'''
 
+# Package imports: os
 import os
 
-def people():
+def people() -> list[str]:
+    '''
+    people() returns an array of file path names for each image in the images/people folder
+    '''
     people = []
     for person in os.listdir("images/people"):
         if person != ".DS_Store":
             people.append("images/people/" + person)
     return people
 
-def pics():
+def pics() -> list[str]:
+    '''
+    pics() returns an array of file path names for each image in the images/pics folder
+    '''
     pics = []
     for person in os.listdir("images/pics"):
         if person != ".DS_Store":
